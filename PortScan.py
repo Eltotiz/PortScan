941ca588bf772713f88e9afa5e6fc5eda10d6998
Eltotiz <77215699+Eltotiz@users.noreply.github.com>
2022-02-09 01:34:15 -0300

    Update PortScan.py

--- PortScan.py
+++ PortScan.py
@@ -20,7 +20,7 @@ print()
 print("Añadirle Nombre al archivo donde se guardara los puertos abiertos.")
 print()
 archivo=input("[+] Inserte nombre al archivo ==> ")
-f = open(f"/PortScan/ports/{archivo}.txt", "w")
+f = open(f"/$HOME/PortScan/ports/{archivo}.txt", "w")
 
 print()
 print("Archivo creado.")
